b43e71cda9559c0d7e36f4756380e7030844e5d1
pegrito-pep <tagnemiguel@gmail.com>
2023-08-22 09:50:13 +0100

    définition des modèles du module auth

---
+++ src/app/models/response.model.ts
@@ -0,0 +1,8 @@
+export class Response{
+    constructor(
+      public id:number,
+      public message:string,
+      public status:string,
+    ){}
+  }
+  
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,14 +1,19 @@
-export interface User {
-  name: string;
-  surname: string;
-  username: string;
-  phone: string;
-  email: string;
-  password?: string;
-  pays?: string;
-  ville?: string;
-  profilImage?: string;
-  enable: boolean;
-  roleId: string;
-  userType: string;
+export class User {
+  constructor(
+    public id:string,
+    public code:number,
+    public name:string,
+    public surname:string,
+    public username:string,
+    public phone:number,
+    public email:string,
+    public password:string,
+    public pays:string,
+    public ville:string,
+    public profilImage:string,
+    public enable:boolean,
+    public roleId:number,
+    public userType:string,
+    public status:number
+  ){}
 }
